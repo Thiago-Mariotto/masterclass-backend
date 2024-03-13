8cacd68b0473dcff67d7f15208db41e653c0fc6b
Thiago Mariotto <thiagomariotto@outlook.com.br>
2024-03-13 14:38:49 -0300

    add project

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const connection = require('./connection');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
+app.post('/users', async (req, res) => {
+  const { name, idade } = req.body;
+
+  if (!name || !idade) {
+    return res.status(400).json({ message: 'Name and age are required' });
+  }
+
+  await connection.execute('INSERT INTO users (name, age) VALUES (?, ?)', [name, idade]);
+  return res.status(201).json({ message: 'User created successfully' });
+});
+
+app.get('/users', async (req, res) => {
+  const [users] = await connection.execute('SELECT * FROM users');
+  return res.status(200).json(users);
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
