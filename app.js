37dc46c8351efa204a7b30987a79caddbfb64a78
Thiago Mariotto <thiagomariotto@outlook.com.br>
2024-03-13 20:21:51 -0300

    add memory db

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const users = [
+  { id: 1, name: 'João', age: 25 },
+  { id: 2, name: 'Maria', age: 30 },
+  { id: 3, name: 'José', age: 40 },
+];
 
 app.use(express.json());
 app.use(cors());
@@ -19,12 +23,14 @@ app.post('/users', async (req, res) => {
     return res.status(400).json({ message: 'Name and age are required' });
   }
 
-  await connection.execute('INSERT INTO users (name, age) VALUES (?, ?)', [name, age]);
+  users.push({ id: users.length + 1, name, age });
+
+  // await connection.execute('INSERT INTO users (name, age) VALUES (?, ?)', [name, age]);
   return res.status(201).json({ message: 'User created successfully' });
 });
 
 app.get('/users', async (req, res) => {
-  const [users] = await connection.execute('SELECT * FROM users');
+  // const [users] = await connection.execute('SELECT * FROM users');
   return res.status(200).json(users);
 });
 
