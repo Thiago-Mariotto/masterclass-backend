54d84b87cf4d32f76feb0beee0bf1c2606a7c25f
Thiago Mariotto <thiagomariotto@outlook.com.br>
2024-03-13 16:37:05 -0300

    add cors

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 
 app.use(express.json());
-app.use(cors({
-  origin: '*',
-  methods: 'GET, POST, PUT, DELETE, OPTIONS',
-}));
+app.use(cors());
 
 app.post('/users', async (req, res) => {
   console.log('new request', req.path, req.method, req.body, new Date());
