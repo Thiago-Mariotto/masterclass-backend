0d22439389a78efe4d308b1e942a50ad4554201a
Thiago Mariotto <thiagomariotto@outlook.com.br>
2024-03-13 16:29:31 -0300

    change cors origin access

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://masterclass-frontend-production.up.railway.app/'
 }));
 
 app.post('/users', async (req, res) => {
